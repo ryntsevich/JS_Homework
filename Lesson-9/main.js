70f1c248a8776d5dde762cbe1d547df3584ccc56
ryntsevich <ryntsevich.oksana@gmail.com>
2020-03-14 16:54:28 +0300

    Homework 9/2.0 + prototype style

--- Lesson-9/main.js
+++ Lesson-9/main.js
@@ -9,10 +9,10 @@ function Animal(name) {
   this.dailyNorm = function(food) {
     if (!arguments.length) return formatFoodAmount();
 
-    if ((food < 50) || (food > 500)) { 
-      
+    if ((food < 50) || (food > 500)) {
+
       return console.log('Введите корректное значение корма!');
-     }
+    }
 
     foodAmount = food;
   };
@@ -42,7 +42,59 @@ function Cat(name) {
 }
 
 var tom = new Cat('Tom');
-//console.log(tom.dailyNorm());
+//console.log(tom.dailyNorm(800));
 //console.log(tom.feed());
 //tom = null;
+console.log(tom.stroke().feed());
+
+
+// Prototype style
+
+function Animal(name) {
+  this._name = name;
+  this._foodAmount = 50;
+
+}
+
+Animal.prototype._formatFoodAmount = function() {
+  return this._foodAmount + ' гр.';
+}
+
+Animal.prototype.dailyNorm = function(food) {
+  if (!arguments.length) return this._formatFoodAmount();
+
+  if ((food < 50) || (food > 500)) {
+
+    return console.log('Введите корректное значение корма!');
+  }
+  this._foodAmount = food;
+}
+
+Animal.prototype.feed = function() {
+  console.log('Насыпаем в миску ' + this.dailyNorm() + ' корма.');
+}
+
+function Cat(name) {
+  Animal.apply(this, arguments);
+}
+
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
+
+Cat.prototype.feed = function() {
+  Animal.prototype.feed.apply(this);
+  console.log('Кот доволен ^_^');
+
+  return this;
+}
+
+Cat.prototype.stroke = function() {
+  console.log('Гладим кота.');
+
+  return this;
+}
+
+var tom = new Cat('Tom');
+
+console.log(tom.dailyNorm(80));
 console.log(tom.stroke().feed());
