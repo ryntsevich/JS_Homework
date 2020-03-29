bd4fb06f2d9446cf8c90ce7285a0855ad1c70d55
ryntsevich <ryntsevich.oksana@gmail.com>
2020-03-29 15:32:05 +0300

    Homework 14 - add practice

---
+++ Lesson-14/Practice/main.js
@@ -0,0 +1,30 @@
+var block = document.getElementById('container');
+var newParagraph = document.createElement('p');
+block.appendChild(newParagraph);
+newParagraph.innerHTML = 'Hello, here are <a href="http://google.by">Link 1 </a> and <a href="http://google.by">Link 2</a>';
+var clonedNewParagraph = newParagraph.cloneNode();
+clonedNewParagraph.innerHTML = 'Hello, here are <a href="http://google.by">Link 3</a> and <a href="http://google.by">Link 4</a>';
+block.appendChild(clonedNewParagraph);
+
+
+var button = document.getElementsByTagName('button')[0];
+button.onmousedown = function() {
+    var linkNewParagraph = newParagraph.children;
+    for (i = 0; i < linkNewParagraph.length; i++) {
+        linkNewParagraph[i].className = 'linkNewParagraph';
+    }
+
+}
+
+var linkCloneNewParagraph = clonedNewParagraph.children;
+for (i = 0; i < linkCloneNewParagraph.length; i++) {
+    linkCloneNewParagraph[i].addEventListener('click', function(event) {
+        event.preventDefault();
+        var target = event.target;
+        var message = target.getAttribute('href');
+        alert(message);
+    });
+}
+
+
+
