fe5d21fa103a661d656ef002491a9712d1929648
ryntsevich <ryntsevich.oksana@gmail.com>
2020-04-12 16:34:05 +0300

    feat: add regex for email validation

--- Lesson-18/main.js
+++ Lesson-18/main.js
@@ -1,4 +1,14 @@
 // Task1
-var email = 'name_surname-12347@gmail.com',
+var email = 'name_surname-1234@gmail.com',
     regexp = /^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@[a-z\d]{1,10}(-?|\.?)[a-z\d]{1,10}\.com$/i;
 console.log(regexp.test(email));
+
+
+// Task2
+function isValidPhoneNumber(str) {
+    return /^(\+?375-?|8-?0)(33|29|44|17|25)-?[1-9]{1}\d{2}-?\d{2}-?\d{2}$/.test(str);
+}
+console.log(isValidPhoneNumber('+375-25-777-77-77'));
+
+// Task3
+
